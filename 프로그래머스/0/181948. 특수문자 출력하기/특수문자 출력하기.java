63f669577b4e156999aeb0240bf1f0acc63a91c4
clipnpaper <147061342+clipnpaper@users.noreply.github.com>
2026-09-03 14:24:37 +0900

    [level 0] Title: 특수문자 출력하기, Time: 106.19 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String readLine;
+        StringTokenizer st;
+
+        // readLine = br.readLine();
+        // st = new StringTokenizer(readLine);
+        // String s = st.nextToken();
+        String answer = "!@#$%^&*(\\'\"<>?:;";
+        // Solution sol = new Solution();
+        // int result = sol.solution(triangle);
+        bw.write(answer + "");
+        bw.close();
+    }
+}

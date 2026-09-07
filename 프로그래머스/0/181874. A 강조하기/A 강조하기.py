d29080ee55676c3cc7daceaf8529e19b19528bdb
clipnpaper <147061342+clipnpaper@users.noreply.github.com>
2026-09-07 20:11:15 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,8 @@
+def solution(myString: str):
+    answer = ''
+    for ch in myString:
+        if ch == 'a' or ch == 'A':
+            answer += ch.upper()
+        else:
+            answer += ch.lower()
+    return answer
